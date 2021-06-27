2dccb8e465d4394665bae65eba9eb2d69ce0b91f
i028 <77450618+i028@users.noreply.github.com>
2021-06-27 17:13:56 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -120,4 +120,4 @@ make && sudo make install
 popd
 
 # Change default shell to zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+#sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
