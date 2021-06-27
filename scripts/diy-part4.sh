4d3bac57a5f01c9591706d021147094cb72994b6
i028 <77450618+i028@users.noreply.github.com>
2021-06-27 21:35:37 +0800

    Create diy-part4.sh

---
+++ scripts/diy-part4.sh
@@ -0,0 +1,10 @@
+cd openwrt/bin/targets/*/*
+rm -rf packages
+rm -rf "*.manifest"
+rm -rf $(find ./ -type f -name "*sha256sums*")
+rename -v "s/config.*/${{ env.iTAG }}.config/" *
+rename -v "s/.*.manifest/${{ env.iTAG }}.manifest/" *
+rename -v "s/.*rootfs/${{ env.iTAG }}-rootfs/" *
+rename -v "s/.*kernel/${{ env.iTAG }}-kernel/" *
+rename -v "s/.*combined/${{ env.iTAG }}/" *
+ls | xargs -i tar zcvf {}.tar.gz {} --remove-files
