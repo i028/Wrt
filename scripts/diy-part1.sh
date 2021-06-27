59f59226eb0bfdaa2f828959c4c6326d7776f718
i028 <77450618+i028@users.noreply.github.com>
2021-06-27 11:38:25 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -15,6 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/i028/small' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/C-oss/helloworld' feeds.conf.default
 
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
