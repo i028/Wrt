fa3b360818d4bb8c3e3991a034de74f7cc23b8b3
i028 <77450618+i028@users.noreply.github.com>
2021-07-03 07:15:15 +0800

    Update diy-part4.sh

--- scripts/diy-part4.sh
+++ scripts/diy-part4.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "iTAG=lede-$(date +"%y.%m.%d%H%M")" >> $GITHUB_ENV
+mkdir -p ./artifact/buildinfo
+cp -rf $(find openwrt/bin/targets/ -type f -name "*.buildinfo" -o -name "*.manifest") ./artifact/buildinfo/
+cd openwrt/bin/targets/*/*
 rm -rf packages
 rm -rf "*.manifest"
 rm -rf $(find ./ -type f -name "*sha256sums*")
@@ -8,6 +12,5 @@ rename -v "s/.*.manifest/${{ env.iTAG }}.manifest/" *
 rename -v "s/.*rootfs/${{ env.iTAG }}-rootfs/" *
 rename -v "s/.*kernel/${{ env.iTAG }}-kernel/" *
 rename -v "s/.*combined/${{ env.iTAG }}/" *
-ls | xargs -i tar zcvf {}.tar.gz {} --remove-files
-
-echo "Success"
+ls | xargs -i tar zcvf {}.gz {} --remove-files
+echo "FIRMWARE=$PWD" >> $GITHUB_ENV
